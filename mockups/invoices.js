e876fda32cc2ee661225f4c8a45117fa73dc7261
Jason Brunelle <jason@jasonbrunelle.com>
2020-06-04 10:28:47 -0500

    commit before OS refresh

--- mockups/invoices.js
+++ mockups/invoices.js
@@ -3,6 +3,24 @@ const new_invoice_row_template = document.getElementById('new-invoice-row');
 const table_body = document.getElementById('table-body');
 const delete_button = document.getElementById('delete-btn');
 
+const vendors = [
+   'Corporate Office Supplies Supplier',
+   'Hercules HVAC',
+   'Lovelace Computer Equipment',
+   'Mustercard',
+   'Phil\'s Water Jugs',
+   'Vending Machine People',
+   'Voltaire\'s Electric',
+];
+
+const gl_codes = [
+    'Beverages',
+    'Shipping',
+    'Office Supplies',
+    'Building Repairs',
+    'Computer Equipment'
+];
+
 const deleteRow = e => {
     e.target.parentElement.parentElement.remove();
 };
